3901f41501f7f531c088d810cc9bad57346e5ee4
Renan Pereira Loureiro <renanpl47@gmail.com>
2022-06-26 01:00:00 -0300

    chore: create account use case

--- setta-desafio-backend/src/modules/accounts/useCases/createAccount/CreateAccountUseCase.js
+++ setta-desafio-backend/src/modules/accounts/useCases/createAccount/CreateAccountUseCase.js
@@ -1,14 +1,24 @@
-import { prisma } from "../../../../database/prisma/prisma.js"
+import { AppError } from "../../../../errors/AppError"
 
 class CreateAccountUseCase {
+  accountsRepository
+  constructor(accountsRepository) {
+    this.accountsRepository = accountsRepository
+  }
   async execute({ name, email, password }) {
-    const user = await prisma.user.create({
+    const accountAlreadyExists = await this.accountsRepository.findByEmail(
+      email
+    )
+    if (accountAlreadyExists) {
+      throw new AppError("Email already register!")
+    }
+    const account = this.accountsRepository.create({
       name,
       email,
       password,
     })
 
-    return user
+    return account
   }
 }
 
