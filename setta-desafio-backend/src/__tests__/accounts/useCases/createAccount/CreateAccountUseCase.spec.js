ed6796db8d96406bd6419c5b81a1276b59e0fe95
Renan Pereira Loureiro <renanpl47@gmail.com>
2022-06-26 00:59:07 -0300

    refactor: expect rejects with AppError instance

--- setta-desafio-backend/src/__tests__/accounts/useCases/createAccount/CreateAccountUseCase.spec.js
+++ setta-desafio-backend/src/__tests__/accounts/useCases/createAccount/CreateAccountUseCase.spec.js
@@ -1,3 +1,4 @@
+import { AppError } from "../../../../errors/AppError.js"
 import { InMemoryAccountRepository } from "../../../../modules/accounts/repositories/inMemoryRepository/InMemoryAccountRepository.js"
 import { CreateAccountUseCase } from "../../../../modules/accounts/useCases/createAccount/CreateAccountUseCase.js"
 
@@ -13,7 +14,7 @@ describe("CreateAccountUseCase", () => {
   it("should be able to create a new account", async () => {
     const account = await createAccountUseCase.execute({
       name: "John Doe",
-      email: "johndoe@gmai.com",
+      email: "johndoe@gmail.com",
       password: "123456",
     })
 
@@ -23,13 +24,13 @@ describe("CreateAccountUseCase", () => {
   it("should not be able to create a new account with already registered", async () => {
     const account = {
       name: "John Doe",
-      email: "johndoe@gmai.com",
+      email: "johndoeTwo@gmail.com",
       password: "123456",
     }
     await createAccountUseCase.execute(account)
 
     expect(async () => {
       await createAccountUseCase.execute(account)
-    }).rejects.toTrow("Email already register!")
+    }).rejects.toEqual(new AppError("Email already register!"))
   })
 })
