86947f572592716221e0462d6672046d1ee81034
Renan Pereira Loureiro <renanpl47@gmail.com>
2022-06-26 12:27:58 -0300

    test: integration /accounts  post endpoint

--- setta-desafio-backend/src/__tests__/accounts/useCases/createAccount/CreateAccountController.spec.js
+++ setta-desafio-backend/src/__tests__/accounts/useCases/createAccount/CreateAccountController.spec.js
@@ -1,15 +1,31 @@
 import request from "supertest"
 import { app } from "../../../../app.js"
 import { prisma } from "../../../../database/prisma/prisma.js"
+import { AccountRepository } from "../../../../modules/accounts/repositories/AccountRepository.js"
+import { CreateAccountUseCase } from "../../../../modules/accounts/useCases/createAccount/CreateAccountUseCase.js"
 
-describe("CreateAccountController", () => {
+let accountsRepository
+let createAccountUseCase
+
+describe("/api/v1/accounts POST ENDPOINT", () => {
+  beforeAll(async () => {
+    accountsRepository = new AccountRepository()
+    createAccountUseCase = new CreateAccountUseCase(accountsRepository)
+
+    await createAccountUseCase.execute({
+      name: "John Doe",
+      email: "john@gmail.com",
+      password: "123456",
+    })
+  })
+
   afterAll(async () => {
     const deleteUser = prisma.user.deleteMany()
     await prisma.$transaction([deleteUser])
     await prisma.$disconnect()
   })
 
-  it("should be able to create a new account", async () => {
+  it("should create a new account", async () => {
     await request(app)
       .post("/api/v1/accounts")
       .send({
@@ -20,6 +36,20 @@ describe("CreateAccountController", () => {
       .expect(201)
       .expect((response) => {
         expect(response.body).toHaveProperty("id")
+      })
+  })
+
+  it("should not create a new account with email already registered", async () => {
+    await request(app)
+      .post("/api/v1/accounts")
+      .send({
+        name: "John Doe",
+        email: "john@gmail.com",
+        password: "123456",
+      })
+      .expect(400)
+      .expect((response) => {
+        expect(response.body).toHaveProperty("error", "Email already register!")
       })
   })
 })
