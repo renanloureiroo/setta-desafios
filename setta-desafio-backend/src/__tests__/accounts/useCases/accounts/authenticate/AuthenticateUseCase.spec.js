62095af442546639e1f0048f7ee6c880ffe2efbb
Renan Pereira Loureiro <renanpl47@gmail.com>
2022-07-03 11:29:08 -0300

    refactor: unit

--- setta-desafio-backend/src/__tests__/accounts/useCases/accounts/authenticate/AuthenticateUseCase.spec.js
+++ setta-desafio-backend/src/__tests__/accounts/useCases/accounts/authenticate/AuthenticateUseCase.spec.js
@@ -6,28 +6,27 @@ import { prisma } from "../../../../../database/prisma/prisma.js"
 
 let usersRepository
 let authenticateUseCase
-let account
 
 describe("AuthenticateUseCase", () => {
   beforeAll(async () => {
     usersRepository = new UsersRepository()
     authenticateUseCase = new AuthenticateUseCase(usersRepository)
-
-    const hashedPassword = await bcryptJs.hash("123456", 8)
-
-    account = await usersRepository.create({
-      name: "John Doe",
-      email: "johndoe@gmail.com",
-      password: hashedPassword,
-    })
   })
 
-  afterAll(async () => {
+  afterEach(async () => {
     await prisma.user.deleteMany()
     await prisma.$disconnect()
   })
 
   it("should be able to authenticate a user", async () => {
+    const hashedPassword = await bcryptJs.hash("123456", 8)
+
+    const account = await usersRepository.create({
+      name: "John Doe",
+      email: "johndoe@gmail.com",
+      password: hashedPassword,
+    })
+
     const response = await authenticateUseCase.execute({
       email: account.email,
       password: "123456",
@@ -37,6 +36,14 @@ describe("AuthenticateUseCase", () => {
   })
 
   it("should not be able with incorrect credentials", async () => {
+    const hashedPassword = await bcryptJs.hash("123456", 8)
+
+    const account = await usersRepository.create({
+      name: "John Doe",
+      email: "johndoe@gmail.com",
+      password: hashedPassword,
+    })
+
     await expect(
       authenticateUseCase.execute({
         email: "incorrectEmail@gmail.com",
@@ -49,6 +56,6 @@ describe("AuthenticateUseCase", () => {
         email: account.email,
         password: "incorrectPassword",
       })
-    ).rejects.toEqual(new AppError("Invalid credentials!", 401))
+    ).rejects.toEqual(new AppError("Invalid credentials!", 400))
   })
 })
--- setta-desafio-backend/src/__tests__/accounts/useCases/accounts/createAccount/CreateUserUseCase.spec.js
+++ setta-desafio-backend/src/__tests__/accounts/useCases/accounts/createAccount/CreateUserUseCase.spec.js
@@ -1,25 +1,25 @@
 import { AppError } from "../../../../../errors/AppError.js"
 import { UsersRepository } from "../../../../../modules/accounts/repositories/UsersRepository.js"
-import { CreateAccountUseCase } from "../../../../../modules/accounts/useCases/createAccount/CreateAccountUseCase.js"
+import { CreateUserUseCase } from "../../../../../modules/accounts/useCases/createUser/CreateUserUseCase.js"
 import { prisma } from "../../../../../database/prisma/prisma.js"
 
-let UsersRepository
-let createAccountUseCase
+let usersRepository
+let createUserUseCase
 
 describe("CreateAccountUseCase", () => {
   beforeAll(() => {
-    UsersRepository = new UsersRepository()
-    createAccountUseCase = new CreateAccountUseCase(UsersRepository)
+    usersRepository = new UsersRepository()
+    createUserUseCase = new CreateUserUseCase(usersRepository)
   })
 
-  afterAll(async () => {
+  afterEach(async () => {
     const deleteAccounts = prisma.user.deleteMany()
     await prisma.$transaction([deleteAccounts])
     await prisma.$disconnect()
   })
 
   it("should be able to create a new account", async () => {
-    const account = await createAccountUseCase.execute({
+    const account = await createUserUseCase.execute({
       name: "John Doe",
       email: "johndoe@gmail.com",
       password: "123456",
@@ -29,14 +29,14 @@ describe("CreateAccountUseCase", () => {
   })
 
   it("should not be able to create a new account with already registered", async () => {
-    await UsersRepository.create({
+    await usersRepository.create({
       name: "John Doe",
       email: "johndoetwo@gmail.com",
       password: "123456",
     })
 
     await expect(
-      createAccountUseCase.execute({
+      createUserUseCase.execute({
         name: "John Doe",
         email: "johndoetwo@gmail.com",
         password: "123456",
